7af80c17f7a686f29e330d1c3a28c413c56b22a7
Christophe Hamerling <christophe.hamerling@gmail.com>
2013-11-27 09:33:57 +0100

    Versions basis

---
+++ sirocco-api-openstack-server/src/main/java/org/ow2/sirocco/cloudmanager/api/openstack/server/resources/nova/Versions.java
@@ -0,0 +1,48 @@
+/**
+ * SIROCCO
+ * Copyright (C) 2013 France Telecom
+ * Contact: sirocco@ow2.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ */
+
+package org.ow2.sirocco.cloudmanager.api.openstack.server.resources.nova;
+
+import org.glassfish.jersey.process.internal.RequestScoped;
+import org.ow2.sirocco.cloudmanager.api.openstack.commons.resource.AbstractResource;
+import org.ow2.sirocco.cloudmanager.api.openstack.commons.resource.ResourceInterceptorBinding;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.core.Response;
+
+import static org.ow2.sirocco.cloudmanager.api.openstack.commons.resource.ResponseHelper.notImplemented;
+
+/**
+ * @author Christophe Hamerling - chamerling@linagora.com
+ */
+@ResourceInterceptorBinding
+@RequestScoped
+public class Versions extends AbstractResource implements org.ow2.sirocco.cloudmanager.api.openstack.nova.resources.Versions {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Versions.class);
+
+    @Override
+    public Response get() {
+        return notImplemented("Versions", "get");
+    }
+
+}
