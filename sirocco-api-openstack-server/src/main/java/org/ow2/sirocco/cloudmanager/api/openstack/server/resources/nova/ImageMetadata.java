10a0e20a61fceb54d908d64800d009133f6e54d0
Christophe Hamerling <christophe.hamerling@gmail.com>
2013-11-28 15:50:25 +0100

    ImageMetadata impl

--- sirocco-api-openstack-server/src/main/java/org/ow2/sirocco/cloudmanager/api/openstack/server/resources/nova/ImageMetadata.java
+++ sirocco-api-openstack-server/src/main/java/org/ow2/sirocco/cloudmanager/api/openstack/server/resources/nova/ImageMetadata.java
@@ -28,6 +28,7 @@ import org.ow2.sirocco.cloudmanager.api.openstack.commons.resource.RequestHelper
 import org.ow2.sirocco.cloudmanager.api.openstack.commons.resource.ResourceInterceptorBinding;
 import org.ow2.sirocco.cloudmanager.api.openstack.nova.model.Metadata;
 import org.ow2.sirocco.cloudmanager.api.openstack.server.functions.MapToMetadata;
+import org.ow2.sirocco.cloudmanager.api.openstack.server.utils.MapHelper;
 import org.ow2.sirocco.cloudmanager.core.api.IMachineImageManager;
 import org.ow2.sirocco.cloudmanager.core.api.exception.CloudProviderException;
 import org.ow2.sirocco.cloudmanager.core.api.exception.ResourceNotFoundException;
@@ -35,7 +36,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 import java.util.Map;
 
@@ -77,16 +77,62 @@ public class ImageMetadata extends AbstractResource implements org.ow2.sirocco.c
 
     @Override
     public Response update(Metadata metadata) {
-        return notImplemented("metadata", "update");
+        try {
+            Map<String, String> meta = manager.getMachineImageById(getImageId()).getProperties();
+            Map<String, String> input = metadata.getMetadata();
+
+            // According to the openstack documentation
+            // update operation will update the properties which are already set and do not modify other
+            // It will not add any metadata which is not already in, we are just in update mode, node add
+            // to add, check the #set method.
+            Map<String, String> updated =  MapHelper.updateMap(meta, input);
+            manager.updateMachineImageAttributes(getImageId(), ImmutableMap.<String, Object>of("properties", updated));
+
+            // FIXME : Need to query the backend to get new values
+            return ok(new MapToMetadata().apply(updated));
+
+        } catch (ResourceNotFoundException rnfe) {
+            return resourceNotFoundException("image", getImageId(), rnfe);
+        } catch (CloudProviderException e) {
+            final String error = "Error while getting image metadata";
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug(error, e);
+            } else {
+                LOGGER.error(error);
+            }
+            return computeFault("Server Error", 500, e.getMessage());
+        }
     }
 
     @Override
     public Response set(Metadata metadata) {
-        return notImplemented("metadata", "set");
+        try {
+            Map<String, String> meta = manager.getMachineImageById(getImageId()).getProperties();
+            Map<String, String> input = metadata.getMetadata();
+
+            // According to the openstack documentation
+            // set operation will replace properties that match incoming request and removes items not specified in the request.
+            Map<String, String> updated = MapHelper.replaceMap(meta, input);
+            manager.updateMachineImageAttributes(getImageId(), ImmutableMap.<String, Object>of("properties", updated));
+
+            // FIXME : Need to query the backend to get new values
+            return ok(new MapToMetadata().apply(updated));
+
+        } catch (ResourceNotFoundException rnfe) {
+            return resourceNotFoundException("image", getImageId(), rnfe);
+        } catch (CloudProviderException e) {
+            final String error = "Error while getting image metadata";
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug(error, e);
+            } else {
+                LOGGER.error(error);
+            }
+            return computeFault("Server Error", 500, e.getMessage());
+        }
     }
 
     @Override
-    public Response get(@PathParam("key") String key) {
+    public Response get(String key) {
         try {
             Map<String, String> meta = manager.getMachineImageById(getImageId()).getProperties();
             if (meta != null && meta.get(key) != null) {
@@ -107,15 +153,57 @@ public class ImageMetadata extends AbstractResource implements org.ow2.sirocco.c
         }    }
 
     @Override
-    public Response set(@PathParam("key") String key, String value) {
-        // TODO : Check how to modify a property in sirocco
-        return notImplemented("image.metadata", "setkey");
+    public Response set(String key, String value) {
+        // get all the values and replace the given one with the given value
+        try {
+            Map<String, String> meta = manager.getMachineImageById(getImageId()).getProperties();
+            if (meta.get(key) != null) {
+                meta.put(key, value);
+                manager.updateMachineImageAttributes(getImageId(), ImmutableMap.<String, Object>of("properties", meta));
+            }
+            // FIXME : What if the input key/value is not available? Add it or not.
+            // The openstack documentation does not specify what to do.
+            // FIXME : Need to query the backend to get new values
+            // FIXME : What is the return code in this case?
+            return ok(new MapToMetadata().apply(meta));
+
+        } catch (ResourceNotFoundException rnfe) {
+            return resourceNotFoundException("image", getImageId(), rnfe);
+        } catch (CloudProviderException e) {
+            final String error = "Error while getting image metadata";
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug(error, e);
+            } else {
+                LOGGER.error(error);
+            }
+            return computeFault("Server Error", 500, e.getMessage());
+        }
     }
 
     @Override
-    public Response delete(@PathParam("key") String key) {
-        // TODO : Check how to delete a property in sirocco
-        return notImplemented("image.metadata", "deletekey");
+    public Response delete(String key) {
+        // get all the values and replace the given one with the given value
+        try {
+            Map<String, String> meta = manager.getMachineImageById(getImageId()).getProperties();
+            if (meta.get(key) != null) {
+                meta.remove(key);
+                manager.updateMachineImageAttributes(getImageId(), ImmutableMap.<String, Object>of("properties", meta));
+                return deleted();
+            } else {
+                // FIXME : What is the return code, not defined in the openstack documentation?
+                return notFound();
+            }
+        } catch (ResourceNotFoundException rnfe) {
+            return resourceNotFoundException("image", getImageId(), rnfe);
+        } catch (CloudProviderException e) {
+            final String error = "Error while getting image metadata";
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug(error, e);
+            } else {
+                LOGGER.error(error);
+            }
+            return computeFault("Server Error", 500, e.getMessage());
+        }
     }
 
     /**
