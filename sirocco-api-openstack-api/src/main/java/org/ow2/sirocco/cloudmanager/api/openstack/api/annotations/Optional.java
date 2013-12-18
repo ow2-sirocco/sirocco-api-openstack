4f7caaf45a09144cfec6b7d4f3bea29853bcaa92
Christophe Hamerling <christophe.hamerling@gmail.com>
2013-12-18 23:39:18 +0100

    optional annotation

---
+++ sirocco-api-openstack-api/src/main/java/org/ow2/sirocco/cloudmanager/api/openstack/api/annotations/Optional.java
@@ -0,0 +1,36 @@
+/**
+ * SIROCCO
+ * Copyright (C) 2013 France Telecom
+ * Contact: sirocco@ow2.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ */
+package org.ow2.sirocco.cloudmanager.api.openstack.api.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Mark an optional property
+ *
+ * @author Christophe Hamerling - chamerling@linagora.com
+ */
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Optional {
+}
